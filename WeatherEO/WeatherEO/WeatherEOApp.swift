f28349cebbf1068372011a5180a743127d4b79bc
Qwertu-Hacker <109757033+Qwertu-Hacker@users.noreply.github.com>
2023-08-31 13:52:36 +0300

    Create empty project

---
+++ WeatherEO/WeatherEO/WeatherEOApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherEOApp.swift
+//  WeatherEO
+//
+//  Created by Igor Kononov on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherEOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
